5a781c19327e9c3365bc60d57f6edf2e383453e6
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2021-02-19 15:23:17 +0100

    Added ways to inherit player class and to test collisions

---
+++ Assets/Scripts/Debug/DebugMover.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using static UnityEngine.Input;
+
+/*
+    Written by F
+    Licensed according to license in Assets/Licenses/Licence-T.txt
+    ***
+    Not meant to make it into any part of the final product
+*/
+
+namespace EDebug
+{
+    public class DebugMover : MonoBehaviour
+    {
+        [SerializeField]
+        float movementScale = 1f;
+        [SerializeField]
+        KeyCode keyLeft = KeyCode.LeftArrow;
+        [SerializeField]
+        KeyCode keyRight = KeyCode.RightArrow;
+        [SerializeField]
+        KeyCode keyUp = KeyCode.UpArrow;
+        [SerializeField]
+        KeyCode keyDown = KeyCode.DownArrow;
+        Rigidbody2D rb = null;
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 newPos = rb.position;
+            if (GetKey(keyLeft))
+            {
+                newPos += new Vector2(-1 * Time.deltaTime * movementScale, 0);
+            }
+            if (GetKey(keyRight))
+            {
+                newPos += new Vector2(1 * Time.deltaTime * movementScale, 0);
+            }
+            if (GetKey(keyUp))
+            {
+                newPos += new Vector2(0, 1 * Time.deltaTime * movementScale);
+            }
+            if (GetKey(keyDown))
+            {
+                newPos += new Vector2(0, -1 * Time.deltaTime * movementScale);
+            }
+            rb.MovePosition(newPos);
+
+        }
+    }
+}
